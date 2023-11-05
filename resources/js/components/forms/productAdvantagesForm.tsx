79e3929effdfe0e7d74ec83a5ad884815acbf7c8
mrtoxas <mrtoxas@gmail.com>
2023-11-05 08:36:18 +0200

    product features draft

--- resources/js/components/forms/productAdvantagesForm.tsx
+++ resources/js/components/forms/productAdvantagesForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { useForm } from "react-hook-form"
+import { useForm, useFieldArray, Validator } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
+import { z } from 'zod';
 import { Trash2Icon, PlusIcon } from "@/components/ui/icons";
 import {
   Form,
@@ -24,41 +24,43 @@ import {
   TableRow
 } from "@/components/shadcn/ui/table";
 
+
 const FormSchema = z.object({
-  name: z.string().min(1),
-  value: z.string().min(1),
-})
+	items: z.array(z.object({
+	  id: z.number(),
+	  name: z.string().min(1),
+	  value: z.string().min(1),
+	})),
+});
 
 export const ProductAdvantagesForm = () => {
-	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [data, setData] = useState({
+		items: [
+			{id: 1, name: 'name1', value: 'value1'},
+			{id: 2, name: 'name2', value: 'value2'},
+			{id: 3, name: 'name3', value: 'value3'}
+		] 
+	});
 
-	 const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-    	name: "",
-      value: ""
-    },
+	const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema)
   })
 
-	function handleSubmit(data: z.infer<typeof FormSchema>) {}
+  const { handleSubmit, control, register, setValue, formState: { isSubmitting } } = form;
 
-	return (
-		<div>
-			<Form {...form}>
-	      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
-	      	<Table>
-					  <TableHeader>
-					    <TableRow className="hover:bg-transparent">
-					      <TableHead className="w-[50%] px-2">Назва</TableHead>
-					      <TableHead className="w-[50%] px-2">Значення</TableHead>
-					      <TableHead></TableHead>
-					    </TableRow>
-					  </TableHeader>
-					  <TableBody>
-					    <TableRow className="hover:bg-transparent">
-					      <TableCell className="px-2">
+  const { fieldArray } = useFieldArray({
+    name: 'items',
+    defaultValue: data.items,
+    register,
+  });
+
+  const preparedItems = useMemo(()=>{
+  	return data.items.map((item, index)=>{
+  		return (
+  			<TableRow className="hover:bg-transparent">
+					      <TableCell className="pl-0 pr-2">
 					      	<FormField
-					          control={form.control}
+					          control={item.control}
 					          name="name"
 					          render={({ field }) => (
 					            <FormItem>
@@ -70,9 +72,9 @@ export const ProductAdvantagesForm = () => {
 					          )}
 					        />
 					      </TableCell>
-					      <TableCell className="px-2">
+					      <TableCell className="pl-2 pr-0">
 					      	<FormField
-					          control={form.control}
+					          control={item.control}
 					          name="value"
 					          render={({ field }) => (
 					            <FormItem>
@@ -84,17 +86,36 @@ export const ProductAdvantagesForm = () => {
 					          )}
 					        />
 					      </TableCell>
-					      <TableCell className="px-2">
+					      <TableCell className="pl-2 pr-0">
 					      	<Button variant="ghost" type="button" className="hover:text-red-600">
 					      		<Trash2Icon className="h-4 w-4" />
 					      	</Button>
 					      </TableCell>
 					    </TableRow>
+  		)
+  	})
+  }, [data])
+
+  
+
+	function onSubmit(data: z.infer<typeof FormSchema>) {}
+
+	return (
+		<div>
+			<Form {...form}>
+	      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+	      	<Table>
+					  <TableHeader>
+					    <TableRow className="hover:bg-transparent">
+					      <TableHead className="w-[50%] pl-0 pr-2">Назва</TableHead>
+					      <TableHead className="w-[50%] pl-2 pr-0">Значення</TableHead>
+					      <TableHead></TableHead>
+					    </TableRow>
+					  </TableHeader>
+					  <TableBody>
+					  	{preparedItems}
 					  </TableBody>
 					</Table>
-
-
-	      	
 	        <div className="mt-4 flex justify-between">           
 		        <Button onClick={handleSubmit} type="submit" disabled={isSubmitting}>
 		          {isSubmitting && <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />} Зберегти
