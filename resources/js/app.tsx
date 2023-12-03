6457ba3076865aa8df02c9ca56690789d9c641b5
mrtoxas <mrtoxas@gmail.com>
2023-12-03 13:48:38 +0200

    added branch to gh-action, fix title

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,7 +6,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ThemeProvider } from './components/theme/theme-provider';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Shmot-Shopy';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
